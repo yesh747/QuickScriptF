0a75b8918ebe688866834fd371abeac75d836d6d
Logan May <logsta.may@gmail.com>
2017-02-18 15:51:14 -0500

    Add Pharm view, rework data, add some state routing

--- app/addPatientView/addPatientView.js
+++ app/addPatientView/addPatientView.js
@@ -9,7 +9,7 @@ angular.module('myApp.addPatientView', ['ngRoute'])
   });
 }])
 
-.controller('AddPatientViewCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+.controller('AddPatientViewCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
 
   // Initialize $scope.data
   $scope.data = {};
@@ -34,12 +34,28 @@ angular.module('myApp.addPatientView', ['ngRoute'])
   // // Form Handler
   $scope.submitForm = function() {
 
+    // Get patient data
     $rootScope.newPatient = {
       name: $scope.patient.firstName + $scope.patient.lastName,
       address: $scope.patient.address,
+      email: $scope.patient.email,
+      phone: $scope.patient.phone,
       ssn: $scope.patient.ssn,
       doB: $scope.patient.birthDate
     }
 
+    // + Add new patient to database
+
+    // Choose route
+    if ($scope.data.cb1 == true) {
+      $rootScope.clickedPatient = $rootScope.newPatient;
+      $rootScope.newPatient = null;
+      $location.path('/addPrescriptionView');
+    }
+    else {
+      // + Add new patient to database
+      $rootScope.newPatient = null;
+      $location.path('/patientView');
+    }
   }
 }]);
--- app/addPrescriptionView/addPrescriptionView.js
+++ app/addPrescriptionView/addPrescriptionView.js
@@ -9,12 +9,64 @@ angular.module('myApp.addPrescriptionView', ['ngRoute'])
   });
 }])
 
-.controller('AddPrescriptionViewCtrl', ['$scope', function($scope) {
+.controller('AddPrescriptionViewCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 
-  $scope.patient = "John";
+  $rootScope.clickedPatient = {};
+  $rootScope.clickedPatient.name = "John"; // temporary holder
+  $rootScope.doctorInformation = {};
+  $rootScope.doctorInformation.name = "Dr. No";
+
+  // Get drugs, schedules, and times for form selectors
   $scope.drugList = ('Xanax Adderall Acetomenphan Belozik Frumuti Sedunkil').split(' ').map(function (drug) { return { name: drug }; });
-  $scope.schedules = ['Daily', 'Every other day', 'Once every three days', 'Once per week',
-                      'MWF', 'TR'].map(function (sched) { return { schedule: sched }; });
+  $scope.schedules = ['1', '2', '3', '4', '5', '6', '7'].map(function (sched) { return { schedule: sched }; });
+   var times = ['Morning', 'Noon', 'Evening'];
+  // if ($scope.prescription.number > 1) $scope.times.push('Morning Noon', 'Noon Evening', 'Morning Evening');
+  times.push('Morning, Noon', 'Noon, Evening', 'Morning, Evening');
+  $scope.times = times.map(function (t) { return { time: t }; });
+  $scope.pharmList = ['CVS', 'Walgreens', 'Walmart'].map(function (p) { return { pharmacy: p }; });
 
+
+  // // Form Handler
+  $scope.submitForm = function() {
+
+  // Helpers
+
+    $scope.today = function() {
+      var date = new Date();
+      var dd = someDate.getDate();
+      var mm = someDate.getMonth() + 1;
+      var y = someDate.getFullYear();
+      var today = dd + '/'+ mm + '/'+ y;
+      return today;
+    }
+
+    $scope.refillDate = function() {
+      var date = new Date();
+      date.setDate(data.getDate() + $scope.numDays);
+      var dd = someDate.getDate();
+      var mm = someDate.getMonth() + 1;
+      var y = someDate.getFullYear();
+      var refillDate = dd + '/'+ mm + '/'+ y;
+      return refillDate;
+    }
+
+    // Set values
+    $scope.newPrescription = {
+      patient: $rootScope.clickedPatient.name,
+      doctor: $rootScope.doctorInformation.name,
+      pharmacy: $scope.prescription.pharmacy,
+      drug: $scope.prescription.name,
+      dosage: $scope.prescription.dosage,
+      dosagePeriod: $scope.prescription.schedule,
+      dosageNumber: $scope.prescription.number,
+      timeOfDay: $scope.prescription.time,
+      numDays: $scope.prescription.numDays,
+      refillDate: $scope.refillDate(),
+      datePrescribed: $scope.today(),
+      dateFilled: null,
+    }
+  }
+
+  // + Submit the data to server
 
 }]);
