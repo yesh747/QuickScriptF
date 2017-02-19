9c8107f6d2aa7ff014af221f4b6d41567af3e087
Yeshwant Chillakuru <Yesh@Yeshwants-MacBook-Pro.local>
2017-02-19 00:21:06 -0500

    merge

--- app/patientsView/patientsView.js
+++ app/patientsView/patientsView.js
@@ -9,7 +9,7 @@ angular.module('myApp.patientsView', ['ngRoute'])
   });
 }])
 
-.controller('PatientsViewCtrl', ['$scope', '$window', '$rootScope', function($scope, $window, $rootScope) {
+.controller('PatientsViewCtrl', ['$scope', '$http', 'SERVER_HOST', '$window', '$rootScope', function($scope, $http, SERVER_HOST, $window, $rootScope) {
 
   $scope.doctor = $rootScope.doctorInformation;
   console.log($scope.doctor);
@@ -26,6 +26,9 @@ angular.module('myApp.patientsView', ['ngRoute'])
     $scope.selectedPatientName = $rootScope.doctorPatientInformation[position].name;
     console.log('Selected Patient: ' + $scope.selectedPatientName);
 
+    $scope.selectedPatientId = $rootScope.doctorPatientInformation[position].id;
+    console.log('Selected Patient Id: ' + $scope.selectedPatientId);
+
     $scope.drugs = $rootScope.doctorPatientInformation[position].prescriptions;
     console.log('Patient Drugs: ' + $scope.drugs);
 
@@ -35,5 +38,17 @@ angular.module('myApp.patientsView', ['ngRoute'])
     $scope.drugs = null;
     $scope.selectedPatientName = null;
   }
+
+  $scope.textPatient= function() {
+    $http.post(SERVER_HOST + 'sms', {id: $scope.selectedPatientId})
+        .then( function(res) {
+          console.log('works: ' + JSON.stringify(res))
+          },
+          function(res){
+            console.log('error: ' + res);
+          }
+        )
+  };
+
 
 }]);
